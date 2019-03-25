438ed5c17dd2ddbd7e0fba893a0f185d69f4422c
Vishnu P <viprahla@cisco.com>
2019-03-25 21:11:56 +0530

    Initial Commit

---
+++ FormProcessing/src/main/java/com/processing/FormProcessing/FormProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.processing.FormProcessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormProcessingApplication.class, args);
+	}
+
+}
---
+++ FormProcessing/src/test/java/com/processing/FormProcessing/FormProcessingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.processing.FormProcessing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FormProcessingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
