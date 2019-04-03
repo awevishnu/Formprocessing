7cd5d187ffa6ac0be5507f52ab52a53c6f5ccff2
Vishnu P <viprahla@cisco.com>
2019-04-03 19:20:56 +0530

    test

---
+++ FormProcessing/src/main/java/com/processing/FormProcessing/config/DemoRootConfig.java
@@ -0,0 +1,41 @@
+package com.processing.FormProcessing.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+
+@SpringBootConfiguration
+@ComponentScan(basePackages = { "com.processing.FormProcessing.model", " com.processing.FormProcessing.services" })
+@EnableAutoConfiguration
+public class DemoRootConfig {
+	@Bean
+	public LocalSessionFactoryBean sessionFactory(DataSource ds) {
+		LocalSessionFactoryBean lsfb = new LocalSessionFactoryBean();
+		lsfb.setDataSource(ds);
+
+		Properties props = new Properties();
+		props.put("hibernate.show_sql", "true");
+		props.put("hibernate.format_sql", "true");
+		props.put("hibernate.use_sql_comments", "true");
+		props.put("hibernate.transaction.factory_class", "org.hibernate.transaction.JDBCTransactionFactory");
+
+		lsfb.setHibernateProperties(props);
+		lsfb.setMappingResources(new String[] { "Customer.hbm.xml" });
+System.out.println("System=====");
+		return lsfb;
+	}
+
+	@Bean(autowire = Autowire.BY_TYPE)
+	public HibernateTemplate hibernateTemplate() {
+		return new HibernateTemplate();
+	}
+}
