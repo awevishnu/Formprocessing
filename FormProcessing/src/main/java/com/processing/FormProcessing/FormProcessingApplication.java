c2b1e47d8eed43f67dfa25450363fa18dbe8c2ac
Vishnu P <viprahla@cisco.com>
2019-04-03 19:22:06 +0530

    test

--- FormProcessing/src/main/java/com/processing/FormProcessing/FormProcessingApplication.java
+++ FormProcessing/src/main/java/com/processing/FormProcessing/FormProcessingApplication.java
@@ -3,7 +3,8 @@ package com.processing.FormProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.processing.FormProcessing.config"})
+//@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class FormProcessingApplication {
 
 	public static void main(String[] args) {
